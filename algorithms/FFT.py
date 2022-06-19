9e90adef2ab7a3cfcb42116b1c667e562c90b626
DarioCi <dariociardielo39@gmail.com>
2022-06-19 20:00:13 +0200

    init

---
+++ algorithms/FFT.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def FFT(x):
+    """
+    A recursive implementation of
+    the 1D Cooley-Tukey FFT, the
+    input should have a length of
+    power of 2.
+    """
+    N = len(x)
+
+    if N == 1:
+        return x
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = \
+            np.exp(-2j * np.pi * np.arange(N) / N)
+
+        X = np.concatenate( \
+            [X_even + factor[:int(N / 2)] * X_odd,
+             X_even + factor[int(N / 2):] * X_odd])
+        return X
